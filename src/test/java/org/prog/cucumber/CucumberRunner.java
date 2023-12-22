bd9f96207d65225e79b9242de4306e9acf335d58
aosipenko <aoskiev@gmail.com>
2023-12-22 20:07:52 +0100

    Session 9: Cucumber BDD

---
+++ src/test/java/org/prog/cucumber/CucumberRunner.java
@@ -0,0 +1,29 @@
+package org.prog.cucumber;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.prog.cucumber.steps.GoogleSteps;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "org.prog.cucumber.steps",
+        plugin = {"pretty",
+                "json:target/cucumber-reports/Cucumber.json",
+                "html:target/cucumber-report.html"
+        }
+)
+public class CucumberRunner extends AbstractTestNGCucumberTests {
+
+    @BeforeSuite
+    public void setUp() {
+        GoogleSteps.webDriver = new ChromeDriver();
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        GoogleSteps.webDriver.quit();
+    }
+}
