c38e187db9aafc5752beda5b34d2d5b8590825ac
aosipenko <aoskiev@gmail.com>
2024-01-08 19:58:18 +0100

    Session 11: Allure testsng reports

--- src/test/java/org/prog/RegressionTest.java
+++ src/test/java/org/prog/RegressionTest.java
@@ -1,5 +1,8 @@
 package org.prog;
 
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Step;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -17,12 +20,40 @@ public class RegressionTest extends BaseTest {
     }
 
     @Test
+    @Severity(SeverityLevel.NORMAL)
     public void myTest() {
-        System.out.println("Hello Test!");
+        printSomething("test 1");
+        printSomething2("test 1");
+        printSomething3("test 1");
+        printSomething4("test 1");
     }
 
     @Test
+    @Severity(SeverityLevel.NORMAL)
     public void myAnotherTest() {
-        System.out.println("Hello Test!");
+        printSomething("test 2");
+        printSomething2("test 2");
+        printSomething3("test 2");
+        printSomething4("test 2");
+    }
+
+    @Step("I am using method 1 to print {s}")
+    private void printSomething(String s){
+        System.out.println(s);
+    }
+
+    @Step("I am using method 2 to print {s}")
+    private void printSomething2(String s){
+        System.out.println(s);
+    }
+
+    @Step("I am using method 3 to print {s}")
+    private void printSomething3(String s){
+        System.out.println(s);
+    }
+
+    @Step("I am using method 4 to print {s}")
+    private void printSomething4(String s){
+        System.out.println(s);
     }
 }
